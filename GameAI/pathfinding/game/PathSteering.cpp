04c5fe8c778c33e69ec75f1f51a5d82f195aa470
Walter Hill <walterhilldev@gmail.com>
2018-11-05 19:03:26 -0500

    some pseudo code & class building
    - included necessary files
    - added seek steering member

--- GameAI/pathfinding/game/PathSteering.cpp
+++ GameAI/pathfinding/game/PathSteering.cpp
@@ -1,10 +1,13 @@
+#include "Game.h"
 #include "component steering/Steering.h"
 #include "component steering/Unit.h"
 #include "component steering/UnitManager.h"
 #include "PathSteering.h"
+#include "Path.h"
+#include "AStarPathfinder.h"
 
 PathSteering::PathSteering(const UnitID& ownerID, const Vector2D& targetLoc, const UnitID& targetID, bool shouldFlee /*= false*/)
-	: Steering()
+	: Steering(), mSeekSteering(ownerID, targetLoc, targetID, shouldFlee)
 {
 	mType = PATH_STEER;
 	setOwnerID(ownerID);
@@ -15,10 +18,21 @@ PathSteering::PathSteering(const UnitID& ownerID, const Vector2D& targetLoc, con
 Steering* PathSteering::getSteering()
 {
 	Vector2D direction;
-	Unit* pOwner = gpGame->getUnitManager()->getUnit(mOwnerID); //TODO(w): change to GameApp obj
+	Unit* pOwner = gpGame->getUnitManager()->getUnit(mOwnerID);
 	PhysicsData physicsData = pOwner->getPhysicsComponent()->getData();
+	Path path;
 
 	//do stuff here
+
+	//calculate path using A*
+	if(availaible path in pool)	//use messaging system
+		path = A*.findPath
+	
+	if(owner near targetLocation && pathindex != path.len) //use distance function 
+		pathindex++
+		mSekkSteering.setTarget(path[0])
+
+	physicsData = mSeekSteering.getSteering().data
 
 	this->mData = physicsData;
 	return this;
--- GameAI/pathfinding/game/PathSteering.h
+++ GameAI/pathfinding/game/PathSteering.h
@@ -2,10 +2,12 @@
 #define PATH_STEER_H
 
 #include "component steering\Steering.h"
+#include "component steering/SeekSteering.h"
 
 class PathSteering : public Steering
 {
 	protected:
+		SeekSteering mSeekSteering;
 
 	public:
 		PathSteering(const UnitID& ownerID, const Vector2D& targetLoc, const UnitID& targetID = INVALID_UNIT_ID, bool shouldFlee = false);
